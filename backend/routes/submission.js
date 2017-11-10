86ee599c3f199343888dd38f421a3ed4c366c3f6
Nigel Campbell <ncampbell7@gatech.edu>
2017-11-10 16:43:55 -0500

    modifications for file upload support.

--- backend/routes/submission.js
+++ backend/routes/submission.js
@@ -3,7 +3,7 @@ var cors = require('cors');
 
 var response_ok = {'success': true}
 var multer = require('multer');
-var upload = multer().single('file1');
+const upload = multer({ dest: 'files/' })
 
 var whitelist = ['http://0.0.0.0:3000', 'http://localhost:3000']
 var corsOptionsDelegate = function (req, callback) {
@@ -41,31 +41,31 @@ module.exports = function(app) {
 	});
 
 	app.post('/api/submission', function(req, res) {
-		if (req.headers['content-type'] == 'application/json') {
-			Submission.create({
-				user : req.body.user,
-				content: {
-					messages : req.body.content.messages,
-					media : req.body.content.media
-				}
-			}, function(err, submission) {
-				if (err) {
-					console.log("1: " + err);
-					res.send(err);
-				}
-				else {
-					res.send(response_ok);
-				}
-			});
+		Submission.create({
+			user : req.body.user,
+			content: {
+				messages : req.body.content.messages,
+				media : req.body.content.media
+			}
+		}, function(err, submission) {
+			if (err) {
+				console.log("1: " + err);
+				res.send(err);
+			}
+			else {
+				res.send(response_ok);
+			}
+		});
+	});
+
+	app.post('/api/files', upload.array('files', 12),  function (req, res) {
+		try  {
+			console.log(req.files);
+			res.json({"success":true, "message":"Upload successful."});
 		}
-		else if (req.headers['content-type'] == 'multipart/form-data') {
-			upload(req, res, function(err) {
-				if (err) {
-					console.log("error occured during upload");
-					return; 
-				}
-			})
-			res.send("multipart form not implemented yet");
+		catch (err) {
+			console.log(err);
+			res.json({"sucess":false, "message": "Upload unsuccessful."})
 		}
 	});
 
